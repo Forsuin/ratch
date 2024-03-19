b7fa3d60d60fc97ca8c8611f0a2255e7cfe9e526
Ian Laidlaw <ian@kaboodles.com>
2024-03-19 14:00:18 -0400

    Ian forgot to push updates again

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,63 @@
 #define FMT_HEADER_ONLY
 
 #include <fmt/core.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+
+#include "scheduler.h"
+
 
 int main()
 {
-    fmt::println("Hello, World");
+
+    Scheduler scheduler;
+
+
+    std::string input;
+    bool shouldClose = false;
+
+    while(!shouldClose) {
+        fmt::print("Command> ");
+        std::getline(std::cin, input, '\n');
+
+        std::stringstream ss(input);
+        std::string word;
+        std::vector<std::string> tokens;
+
+        while(ss >> word) {
+            tokens.push_back(word);
+        }
+
+
+        if(tokens[0] == "start") {
+
+        }
+        else if(tokens[0] == "step") {
+
+        }
+        else if(tokens[0] == "stepBurst") {
+
+        }
+        else if(tokens[0] == "setMemory") {
+
+        }
+        else if(tokens[0] == "getMemory") {
+
+        }
+        else if(tokens[0] == "run") {
+
+        }
+        else if(tokens[0] == "addProgram") {
+
+        }
+        else {
+            fmt::println("Invalid commands");
+        }
+
+        if(tokens[0] == "quit") {
+            shouldClose = true;
+        }
+    }
 }
--- src/scheduler.h
+++ src/scheduler.h
@@ -2,8 +2,29 @@
 #ifndef RATCH_SCHEDULER_H
 #define RATCH_SCHEDULER_H
 
+#include <queue>
+
+
+struct Task {
+    std::string name;
+    int time_req;
+    int mem_req;
+    int io_delay;
+    int io_req;
+    int execution_time;
+};
+
 
 class Scheduler {
+    int tick_count = 0;
+    int step = 4;
+    int total_memory = 4;
+    int used_memory = 0;
+
+    std::queue<Task> active_tasks;
+    std::queue<Task> finished_tasks;
+
+public:
 
 };
 
